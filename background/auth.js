a69638f98a74646a80bc3c2291fbb331208b6074
Will-Helliwell <wj.helliwell@gmail.com>
2025-02-17 08:44:28 +0800

    Add comment.

--- background/auth.js
+++ background/auth.js
@@ -2,6 +2,15 @@ const CLIENT_ID = "c68135e240a7e3c9e314171a1acec86e";
 const REDIRECT_URL = "https://example.com/";
 const STATE = "123";
 
+/**
+ * Initiates the OAuth authentication process using Chrome's identity API.
+ * 
+ * This function launches a web authentication flow for the TSheets API, prompting
+ * the user to authorize access. If successful, it retrieves the authorization code
+ * from the redirect URL.
+ * 
+ * @returns {void}
+ */
 function authenticateUser() {
     chrome.identity.launchWebAuthFlow(
         {
