9fba5884d25b3d41f00ddce83e44fe1ba2223ca2
Will-Helliwell <wj.helliwell@gmail.com>
2025-04-16 10:43:02 +0800

    Refactor - move jobcodes and timesheets pricessing logic to helper file.

--- background/background.js
+++ background/background.js
@@ -2,5 +2,6 @@ importScripts(
   "/background/localStorage.js",
   "/background/api.js",
   "/background/auth.js",
+  "/background/jobcodesAndTimesheets.js",
   "/background/activeTimesheets.js"
 );
